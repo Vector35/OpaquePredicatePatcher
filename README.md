af4031abf8a8092b7696fc413f8cccb188dcc5f5
Jordan Wiens <jordan@psifertex.com>
2017-10-01 16:19:00 -0400

    format as a plugin

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# OpaquePredicatePatcher
+# OpaquePredicatePatcher (v1.0 alpha)
+Author: **Vector 35 LLC**
 
-This script will automatically remove conditional branches which have constant conditions. It can be run from the command line or installed as a plugin in your user plugin directory.
+_Automatically patch opaque predicates_
 
-# Version Requirement
+## Description:
 
-This script requires Binary Ninja version 1.1.945 or greater.
+This script will automatically remove conditional branches which have constant conditions. It can be run from the command line via `python -mOpaquePredicatePatcher.__main__` (if the BinaryNinja python library is in your path) or installed as a plugin in your user plugin directory. Useful for analyzing obfsucated malware.
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+ * dev - 1.0.dev-945
+
+
+## Required Dependencies
+
+The following dependencies are required for this plugin:
+
+
+
+## License
+
+This plugin is released under a [MIT](LICENSE) license.
+
+
---
+++ __init__.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# Copyright (c) 2015-2017 Vector 35 LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import sys
+
+from binaryninja.log import log_info
+from binaryninja.binaryview import BinaryViewType
+from binaryninja.plugin import PluginCommand
+from binaryninja.plugin import BackgroundTaskThread
+from binaryninja.enums import (MediumLevelILOperation, RegisterValueType)
+
+
+def patch_opaque_inner(bv, status):
+    bv.update_analysis_and_wait()
+    patch_locations = []
+    for i in bv.mlil_instructions:
+        # Allow the UI to cancel the action
+        if status.cancelled:
+            break
+
+        if i.operation != MediumLevelILOperation.MLIL_IF:
+            continue
+        # Get the possible_values of the condition result
+        condition_value = i.condition.possible_values
+        if i.address == 0x40051e:
+            log_info("Address: %x %s" % (i.address, str(condition_value)))
+        # If the condition never changes then its safe to patch the branch
+        if condition_value.type == RegisterValueType.ConstantValue:
+            if condition_value.value == 0 and bv.is_never_branch_patch_available(i.address):
+                patch_locations.append((i.address, True))
+            elif bv.is_always_branch_patch_available(i.address):
+                patch_locations.append((i.address, False))
+
+    return patch_locations
+
+
+def patch_opaque(bv, status):
+    analysis_pass = 0
+    while True:
+        analysis_pass += 1
+        patch_locations = patch_opaque_inner(bv, status)
+        if len(patch_locations) == 0 or analysis_pass == 10 or status.cancelled:
+            break
+        for address, always in patch_locations:
+            if always:
+                log_info("Patching instruction {} to never branch.".format(hex(address)))
+                bv.never_branch(address)
+            else:
+                log_info("Patching instruction {} to always branch.".format(hex(address)))
+                bv.always_branch(address)
+
+
+class PatchOpaqueInBackground(BackgroundTaskThread):
+    def __init__(self, bv, msg):
+        BackgroundTaskThread.__init__(self, msg, True)
+        self.bv = bv
+
+    def run(self):
+        patch_opaque(self.bv, self)
+
+
+def patch_opaque_in_background(bv):
+    background_task = PatchOpaqueInBackground(bv, "Patching opaque predicates")
+    background_task.start()
+
+
+def main():
+    bv = BinaryViewType.get_view_of_file(sys.argv[1])
+    if bv is None:
+        print "Couldn't open %s" % sys.argv[1]
+        return
+
+    patch_opaque(bv)
+
+    dbname = sys.argv[1]
+    if not dbname.endswith(".bndb"):
+        dbname += ".bndb"
+
+    bv.create_database(dbname)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    PluginCommand.register("Patch Opaque Predicates", "Find branches which are unnecessary and remove them",
+        patch_opaque_in_background)
